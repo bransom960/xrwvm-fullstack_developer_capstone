9aedfee7d09c973c70cdcedbdb7e56a3a4ea3fb8
bransom960 <brandi.n.ransom@gmail.com>
2024-04-15 18:17:10 -0400

    week 3

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
+    path(route='logout', view=views.logout_request, name='logout'),
+    path(route='register', view=views.registration, name='register'),
     path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for dealer reviews view
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
